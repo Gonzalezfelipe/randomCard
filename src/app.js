3cbd5aa044cc2cf8fa9c5565a7e86ce33795d11f
Felipe González <160081406+Gonzalezfelipe@users.noreply.github.com>
2024-05-09 23:40:52 +0000

    css ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+let simbols = ["♦", "♥", "♠", "♣"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function randomCard() {
+  let singleNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+  let singleSimbols = simbols[Math.floor(Math.random() * simbols.length)];
+  let card = document.getElementsByClassName("card");
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   randomCard();
+// };
